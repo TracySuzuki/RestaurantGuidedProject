a9f3ea9a671a3a41403f1d73264e134a15d17a88
student15 <student15@student15s-macbook-air.local>
2019-04-27 12:12:39 -0500

    Made requests with URL Session part 1

--- RestaurantGuidedProject/MenuController.swift
+++ RestaurantGuidedProject/MenuController.swift
@@ -14,13 +14,29 @@ class MenuController {
     //a method that has one parameter with a completion closre an array of strings
     func fetchCategories(completion: @escaping ([String]?) -> Void)
     {
+        let categoryURL = baseURL.appendingPathComponent("categories")
+        let task = URLSession.shared.dataTask(with: categoryURL)
+        { (data, response, error) in
+            
+        }
+        task.resume()
     }
     //deserialize each dictionary into a menu item object. The method will perform the request to 2 parameters, the catagory string and in the completion closure a string of array menu items.
     func fetchMenuItems(forCategory categoryName: String, completion: @escaping ([MenuItem]?) -> Void) {
+        let initialMenuURL = baseURL.appendingPathComponent("menu")
+        var components = URLComponents(url: initialMenuURL, resolvingAgainstBaseURL: true)!
+        components.queryItems = [URLQueryItem(name: "category", value: categoryName)]
+        let menuURL = components.url!
+        let task = URLSession.shared.dataTask(with: menuURL)
+        { (data, response, error) in
+            
+        }
+        task.resume()
         
     }
     //Method to post an order has a collection of menu item IDs, includes also an integer specifying the number of minutes the order will take to prep. For both of those the method needs 2 parameters.
     func submitOrder(forMenuIDs menuIds: [Int], completion: @escaping (Int?) -> Void) {
+      let orderURL = baseURL.appendingPathComponent("order")
         
     }
     
