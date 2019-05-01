007bc14811686e8d6469630f36f7bbd03b5ac45f
student15 <student15@student15s-macbook-air.local>
2019-05-01 01:23:26 -0500

    First part of displaying the order

--- RestaurantGuidedProject/OrderTableViewController.swift
+++ RestaurantGuidedProject/OrderTableViewController.swift
@@ -9,38 +9,28 @@
 import UIKit
 
 class OrderTableViewController: UITableViewController {
+    var order = Order()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return order.menuItems.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OrderCellIdentifier", for: indexPath)
+        configure(cell, forItemAt: indexPath)
         return cell
     }
-    */
+    
+    func configure(_ cell: UITableViewCell, forItemAt indexPath: IndexPath) {
+        let menuItem = order.menuItems[indexPath.row]
+        cell.textLabel?.text = menuItem.name
+        cell.detailTextLabel?.text = String(format: "$%.2f", menuItem.price)
+    }
 
     /*
     // Override to support conditional editing of the table view.
