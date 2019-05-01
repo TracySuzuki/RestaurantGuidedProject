365dcec0e292bffcb05ada51a86fc24f8f52cd0d
student15 <student15@student15s-macbook-air.local>
2019-04-30 23:32:04 -0500

    Created outlets with code in MenuItemDetailViewController to connect to the menu detail view to display data in the custom interface

--- RestaurantGuidedProject/MenuItemDetailViewController.swift
+++ RestaurantGuidedProject/MenuItemDetailViewController.swift
@@ -9,25 +9,28 @@
 import UIKit
 
 class MenuItemDetailViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var detailTextLabel: UILabel!
+    @IBOutlet weak var addToOrderButton: UIButton!
+    
     //property below holds the menuItem it recieves from the menuTableViewController
     var menuItem: MenuItem!
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        titleLabel.text = menuItem.name
+        priceLabel.text = String(format: "$%.2f", menuItem.price)
+        detailTextLabel.text = menuItem.detailText
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
